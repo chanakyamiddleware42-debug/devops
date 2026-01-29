542466866f4beadefc3faa76779fe43fa7b50713
chanakyamiddleware42-debug <chanakya.middleware42@gmail.com>
2026-01-29 17:44:03 +0530

    basic shell script

---
+++ GM.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+PERSON1= Narayanareddy
+PERSON2= chanakyaputhalapattu
+
+echo "$PERSON1:: Hi $PERSON2. Goodmorning."
+echo "$PERSON2:: Hi $PERSON1. Goodmorning."
+echo "$PERSON1:: How are you "
+echo "$PERSON2:: i'm doing, Thanks for asking. How are you."
+echo "$PERSON1:: I'm also doing great."
+
